170c713b8bceaad041de4aa4ef2f751e627e87f8
Abhishek Garg <abhishekgarg2009@gmail.com>
2018-06-15 09:49:57 +0530

    fit matters init

---
+++ object_size.py
@@ -0,0 +1,263 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+MASK_COLOR = (0.0,0.0,1.0)
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+ 
+
+fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+# fgbg = cv2.createBackgroundSubtractorMOG2(history=2000, varThreshold=16, detectShadows=True)
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+ap.add_argument("-w", "--width", type=float, required=True,
+	help="width of the left-most object in the image (in inches)")
+args = vars(ap.parse_args())
+# load the image, convert it to grayscale, and blur it slightly
+image = cv2.imread(args["image"])
+cv2.imshow("original",image);
+
+ #fgmask = fgbg.apply(image)
+
+# cv2.imshow('fgmask',image)
+# cv2.imshow('frame',fgmask)
+
+size = image.shape[:2] 
+h = size[0]-1
+w = size[1]-1
+print(h, w)
+colorTopLeft = image[0,0]
+colorTopRight = image[0,w]
+diff = np.subtract(colorTopRight.astype(np.int16), colorTopLeft.astype(np.int16))
+weightedTopColor = np.divide(diff.astype(np.float64), float(w))
+
+# image = cv2.copyMakeBorder(image, 10, 0, 0, 0, cv2.BORDER_CONSTANT, value= colorTop.tolist())
+
+for y in range(0, 10):
+	prev = colorTopLeft.astype(np.float64)
+	for x in range(0, w):
+		prev = np.add(prev.astype(np.float64), weightedTopColor.astype(np.float64))
+		image[y, x] = prev
+
+colorBottomLeft = image[h,0]
+colorBottomRight = image[h,w]
+diff2 = np.subtract(colorBottomRight.astype(np.int16), colorBottomLeft.astype(np.int16))
+weightedBottomColor = np.divide(diff2.astype(np.float64), float(w))
+
+for y in range(h-5, h+1):
+	prev = colorBottomLeft.astype(np.float64)
+	for x in range(0, w):
+		prev = np.add(prev.astype(np.float64), weightedBottomColor.astype(np.float64))
+		image[y, x] = prev		
+
+# colorBottom = image[size[0]-1, size[1]-1]
+# image = cv2.copyMakeBorder(image, 0, 10, 0, 0, cv2.BORDER_CONSTANT, value= colorBottom.tolist())
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+gray = cv2.GaussianBlur(image, (7, 7), 0)
+ 
+# perform edge detection, then perform a dilation + erosion to
+# close gaps in between object edges
+edged = cv2.Canny(gray, 50, 100)
+cv2.imshow("afterEdgeDetection",edged);
+
+edged = cv2.dilate(edged, None, iterations=1)
+cv2.imshow("afterDilation",edged);
+
+edged = cv2.erode(edged, None, iterations=1)
+ 
+cv2.imshow("afterErosion", edged);
+cv2.waitKey(0)
+
+
+
+
+# Face recognition logic
+
+cascPath = "haarcascade_frontalface_default.xml"
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(
+	gray,
+	scaleFactor=1.1,
+	minNeighbors=5,
+	minSize=(30, 30)
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+image_face = image
+
+maxFaceHeight = 0
+maxFaceWidth = 0
+FaceDepth = 0
+
+for (x, y, w, h) in faces:
+	if h >= maxFaceHeight:
+		maxFaceHeight = h
+		maxFaceWidth = w
+		FaceDepth = y + h
+	print("face width in pixels: ", w)
+	print("face height in pixels: ", h)
+	cv2.rectangle(image_face, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image_face)
+cv2.waitKey(0)
+
+
+
+
+
+# find contours in the edge map
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+ 
+# sort the contours from left-to-right and initialize the
+# 'pixels per metric' calibration variable
+(cnts, _) = contours.sort_contours(cnts)
+pixelsPerMetric = None
+
+# loop over the contours individually
+
+# for c in cnts:
+# 	# if the contour is not sufficiently large, ignore it
+# 	print cv2.contourArea(c)
+
+selected_height = 1
+selected_width = 1
+
+count = 1
+ppm = 1
+for c in cnts:
+	# if the contour is not sufficiently large, ignore it
+	# print cv2.contourArea(c)
+	if cv2.contourArea(c) < 400:
+		continue
+
+ 
+	# compute the rotated bounding box of the contour
+	orig = image.copy()
+	box = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
+
+ 
+	# order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	box = perspective.order_points(box)
+	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+ 
+	# loop over the original points and draw them
+	for (x, y) in box:
+		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+# unpack the ordered bounding box, then compute the midpoint
+	# between the top-left and top-right coordinates, followed by
+	# the midpoint between bottom-left and bottom-right coordinates
+	(tl, tr, br, bl) = box
+	(tltrX, tltrY) = midpoint(tl, tr)
+	(blbrX, blbrY) = midpoint(bl, br)
+ 
+	# compute the midpoint between the top-left and top-right points,
+	# followed by the midpoint between the top-righ and bottom-right
+	(tlblX, tlblY) = midpoint(tl, bl)
+	(trbrX, trbrY) = midpoint(tr, br)
+ 
+	# draw the midpoints on the image
+	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+ 
+	# draw lines between the midpoints
+	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+		(255, 0, 255), 2)
+	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+		(255, 0, 255), 2)
+# compute the Euclidean distance between the midpoints
+	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+	print(dA)
+	print(dB)
+
+	if dA > selected_height:
+		selected_height = dA
+		selected_width = dB
+		tl1 = tl
+		tr1 = tr
+		br1 = br
+		bl1 = bl
+ 
+	# if the pixels per metric has not been initialized, then
+	# compute it as the ratio of pixels to supplied metric
+	# (in this case, inches)
+	if pixelsPerMetric is None:
+		pixelsPerMetric = dB / args["width"]
+		ppm = pixelsPerMetric
+# compute the size of the object
+	dimA = dA / pixelsPerMetric
+	dimB = dB / pixelsPerMetric
+
+	print(count)
+	print("height: " , dimA , "shoulder width: ", dimB)
+	count = count+1
+ 
+	# draw the object sizes on the image
+	cv2.putText(orig, "{:.1f}cm".format(dimA),
+		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+		0.65, (255, 255, 255), 2)
+	cv2.putText(orig, "{:.1f}cm".format(dimB),
+		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+		0.65, (255, 255, 255), 2)
+ 
+	# show the output image
+	cv2.imshow("Image", orig)
+	cv2.waitKey(0)
+
+tl1[1] = max(tl1[1], FaceDepth)
+tr1[1] = max(tr1[1], FaceDepth)
+(tltrX1, tltrY1) = midpoint(tl1, tr1)
+(blbrX1, blbrY1) = midpoint(bl1, br1)
+(tlblX1, tlblY1) = midpoint(tl1, bl1)
+(trbrX1, trbrY1) = midpoint(tr1, br1)
+dA1 = dist.euclidean((tltrX1, tltrY1), (blbrX1, blbrY1))
+dB1 = dist.euclidean((tlblX1, tlblY1), (trbrX1, trbrY1))
+print(dA1)
+print(dB1)
+dimA1 = dA1 / ppm
+dimB1 = dB1 / ppm
+print("face height: ", float(maxFaceHeight)/ppm, "  face width : ", float(maxFaceWidth)/ppm)
+print("actual height: " , dimA1 , " actual shoulder width: ", dimB1)
+
+file = open("hw.txt","w")
+file.write(str(dimA1))
+file.write("\n")
+file.write(str(dimB1))
+file.close()
+
+ # if pixelsPerMetric is None:
+ # 		pixelsPerMetric = dB / args["width"]
+
+ # resizedHeight = selected_height/ pixelsPerMetric
+ # resizedWidth = selected_width / pixelsPerMetric
+
+# print("body height: ", resizedHeight, " width: ", resizedWidth)
+
+# resizedHeight = (selected_height - maxFaceHeight)/ pixelsPerMetric
+
+# print("body - face height: ", resizedHeight, " width : ", resizedWidth)
---
+++ server.py
@@ -0,0 +1,277 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import json
+import datetime
+from datetime import datetime, timedelta
+from werkzeug import secure_filename
+import os
+import subprocess
+from flask_cors import CORS
+
+
+now = datetime.today()
+
+print(now)
+
+epoch = datetime.utcfromtimestamp(0)
+
+app = Flask(__name__)
+CORS(app)
+
+sizes = ["S", "M", "L", "XL", "XXL"]
+
+class Listing:
+    def __init__(self, brand, size, category):
+        self.brand = brand
+        self.size = size
+        self.category = category
+        # self.url = url
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+
+
+class listingResponse:
+    def __init__(self,name,size):
+        self.url = "https://i2.wp.com/paristylebd.com/wp-content/uploads/2016/01/black-formal-2.jpg"
+        self.name = name
+        self.sizes = sizes
+        self.activeSize = size
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+
+class Response:
+    def __init__(self, listingResponse):
+        self.listings = listingResponse
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+
+class SizeMeasurements:
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+
+class Order:
+    def __init__(self,id, item, rating, isreturn, orderPlaced):
+        self.id = id
+        self.item = item
+        self.rating = rating
+        self.isreturn = isreturn
+        self.orderPlaced = orderPlaced
+
+
+userSize = {}
+userSize['Order_History'] = "38_27"
+userSize['Photo'] = "37_27"
+userSize['Both'] = "37_27"
+
+listings = [Listing("Reebok","S","shirt"),Listing("Reebok","M","shirt"),Listing("Reebok","L","shirt"),Listing("Nike","S","shirt"),Listing("Nike","M","shirt"),Listing("Nike","L","shirt")]
+
+
+measurement2size = {}
+measurement2size["48_69"] = "Reebok_S"
+measurement2size["50_69"] = "Reebok_M"
+measurement2size["51_71"] = "Reebok_L"
+measurement2size["42_67"] = "Nike_S"
+measurement2size["43_69"] = "Nike_M"
+measurement2size["45_71"] = "Nike_L"
+
+
+size2measurements = {}
+for key in measurement2size:
+    size2measurements[measurement2size[key]] = key
+
+orders = [Order(1,"Reebok_S",2,False, (now - timedelta(days=60)))  , Order(2,"Reebok_M",5,False, (now - timedelta(days=30))) ,Order(3,"Nike_L",4,False,(now - timedelta(days=40))) ,Order(4,"Reebok_L",3,False, (now - timedelta(days=10))) ,Order(5,"Nike_S",1,False, (now - timedelta(days=8))) ,Order(6,"Nike_M",4,False, (now - timedelta(days=3)))]
+
+order2Score = {}
+orderId2item = {}
+bestScore = -100
+bestOrder = 0
+for order in orders:
+    score = order.rating * 0.8
+    months = (now - order.orderPlaced).days / float(30)
+    print(months)
+    score = score + (0.2 / months)
+    order2Score[order.id] = score
+    if score > bestScore:
+        bestOrder = order.id
+    orderId2item[order.id] = order.item
+
+print(order2Score)
+print(orderId2item[bestOrder])
+
+avgWidth = 0
+avgLength = 0
+scoreSum = 0
+for orderId in order2Score:
+    scoreSum = scoreSum + order2Score[orderId]
+    measure = size2measurements[orderId2item[orderId]]
+    avgWidth = avgWidth +  int(measure.split("_")[0])*order2Score[orderId]
+    avgLength = avgLength +  int(measure.split("_")[1])*order2Score[orderId]
+
+print(avgWidth/scoreSum)
+print(avgLength/scoreSum)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+
+UPLOAD_FOLDER = '/Users/abhishek.krishan/scripts/hackday'
+
+
+
+@app.route('/upload', methods = ['POST'])
+def api_root():
+    if request.method == 'POST' and request.files['image']:
+        img = request.files['image']
+        print(img.filename)
+        img_name = secure_filename(img.filename)
+        saved_path = os.path.join(UPLOAD_FOLDER, img_name)
+        img.save(saved_path)
+        completed = subprocess.run(['python3.6', 'object_size.py','-i',saved_path,'-w',"8.56"])
+        file = open("hw.txt","r")
+        data = file.readlines()
+        w = int(float(data[0]))
+        l = int(float(data[1]))
+        userSize['Photo'] = str(w) + '_' + str(l)
+        #subprocess.run(["python3.6 object_size.py -i ", saved_path + " -w 3.37"])
+        return "DONE"
+        # return send_from_directory(UPLOAD_FOLDER,img_name, as_attachment=True)
+    else:
+        return "Where is the image?"
+
+
+
+@app.route("/updateSize")
+def updateSize():
+    args = request.args
+    width = args['width']
+    length = args['length']
+    userSize['IMAGE'] = str(width) + '_' + str(length)
+    return "DONE"
+
+
+def getMySizeFromMeasurements(size):
+
+    mywidth = int(size.split("_")[0])
+    mylength = int(size.split("_")[1])
+    brandSizeToDiff = {}
+
+    for key in measurement2size:
+        print(key)
+
+        width = int(key.split("_")[0])
+        length = int(key.split("_")[1])
+
+        widthDiff = mywidth - width
+        lengthDiff = mylength - length
+
+        value = measurement2size[key]
+
+        brand = value.split("_")[0]
+        s = value.split("_")[1]
+
+        print(value)
+
+        if brandSizeToDiff.get(brand) is None: 
+            brandSizeToDiff[brand] = s + "_" + str(widthDiff) + '_' + str(lengthDiff)
+        else:
+            prevValue = brandSizeToDiff[brand]
+
+            prevsize = prevValue.split("_")[0]
+            prevWidthDiff = int(prevValue.split("_")[1])
+            prevLengthDiff = int(prevValue.split("_")[2])
+
+            diff = widthDiff + lengthDiff
+            prevDiff = prevWidthDiff + prevLengthDiff
+            if ( abs(prevDiff) - abs(diff) > 0 and prevDiff * diff > 0 ) or (prevDiff * diff < 0 and diff > 0) or ( prevDiff == 0 and ((prevWidthDiff < 0 and widthDiff > 0)  or (prevWidthDiff> 0 and widthDiff < prevWidthDiff))) or (diff == 0 and widthDiff == 0 and lengthDiff == 0 ):
+                brandSizeToDiff[brand] = s + "_" + str(widthDiff) + '_' + str(lengthDiff)
+
+        print(brandSizeToDiff)
+
+      
+    print (brandSizeToDiff)
+
+    lists = []
+    for key in brandSizeToDiff:
+        lists.append(listingResponse(key + "_" + brandSizeToDiff[key].split("_")[0], brandSizeToDiff[key].split("_")[0]))
+        # lists.append(Listing(key,brandSizeToDiff[key].split("_")[0],"tshirt"))
+
+    return lists
+
+
+
+@app.route("/userSizes")
+def userSizes():
+    return jsonify(userSize) 
+
+
+@app.route("/catalogue/products")
+def list():
+    args = request.args
+    print (args)
+    size = args.get('size')
+    sizeType = args.get('pref')
+    brand = args.get('brand')
+    results = []
+    results1 = []
+    print("sagar")
+    print(size)
+    print(brand)
+
+    newL = []
+    for l in listings:
+        newL.append(listingResponse(l.brand + "_" + l.size,l.size))
+
+    if brand is None and sizeType is None and size is None:
+        return Response(newL).toJSON()
+
+
+    if sizeType is not None:
+        print(sizeType)
+        measurements = userSize[sizeType]
+        print(measurements)
+        brandWithSizes = getMySizeFromMeasurements(measurements)
+
+        print(brandWithSizes)
+        if brand is not None:
+            for b in brandWithSizes:
+                if b.name.split("_")[0] == brand:
+                    results1.append(b)
+        else:
+            results1 = brandWithSizes
+        print(results1)
+    else:   
+        if brand is not None: 
+            for listing in newL:
+                if listing.name.split("_")[0] == brand:
+                    results.append(listing)
+        else:
+            results = newL
+        
+        if size is not None:
+            for result in results:
+                print(result.activeSize)
+                if result.activeSize == size:
+                    results1.append(result)
+        else: 
+            results1 = results
+
+    return Response(results1).toJSON()
+
+
+
+if __name__ == '__main__':
+    app.run(port=int("8080"),debug=True)
