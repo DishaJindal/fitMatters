c9dfdf24a4607497c3ce3218b2931c1cb5929e58
Abhishek Garg <abhishekgarg2009@gmail.com>
2018-06-15 10:36:38 +0530

    Added func to draw box around detected box

--- object_size.py
+++ object_size.py
@@ -6,6 +6,7 @@ import numpy as np
 import argparse
 import imutils
 import cv2
+import os
 
 MASK_COLOR = (0.0,0.0,1.0)
 def midpoint(ptA, ptB):
@@ -98,7 +99,7 @@ faces = faceCascade.detectMultiScale(
 
 print("Found {0} faces!".format(len(faces)))
 
-image_face = image
+image_face = image.copy()
 
 maxFaceHeight = 0
 maxFaceWidth = 0
@@ -202,6 +203,7 @@ for c in cnts:
 		tr1 = tr
 		br1 = br
 		bl1 = bl
+		box1 = box
  
 	# if the pixels per metric has not been initialized, then
 	# compute it as the ratio of pixels to supplied metric
@@ -229,25 +231,42 @@ for c in cnts:
 	cv2.imshow("Image", orig)
 	cv2.waitKey(0)
 
-tl1[1] = max(tl1[1], FaceDepth)
-tr1[1] = max(tr1[1], FaceDepth)
+final_img = image.copy()
+tl1[1] = max(tl1[1], 1.2*FaceDepth)
+tr1[1] = max(tr1[1], 1.2*FaceDepth)
+cv2.drawContours(final_img, [box1.astype("int")], -1, (0, 255, 0), 2)
 (tltrX1, tltrY1) = midpoint(tl1, tr1)
 (blbrX1, blbrY1) = midpoint(bl1, br1)
 (tlblX1, tlblY1) = midpoint(tl1, bl1)
 (trbrX1, trbrY1) = midpoint(tr1, br1)
+cv2.circle(final_img, (int(tltrX1), int(tltrY1)), 5, (255, 0, 0), -1)
+cv2.circle(final_img, (int(blbrX1), int(blbrY1)), 5, (255, 0, 0), -1)
+cv2.circle(final_img, (int(tlblX1), int(tlblY1)), 5, (255, 0, 0), -1)
+cv2.circle(final_img, (int(trbrX1), int(trbrY1)), 5, (255, 0, 0), -1)
+cv2.line(final_img, (int(tltrX1), int(tltrY1)), (int(blbrX1), int(blbrY1)), (255, 0, 255), 2)
+cv2.line(final_img, (int(tlblX1), int(tlblY1)), (int(trbrX1), int(trbrY1)), (255, 0, 255), 2)
 dA1 = dist.euclidean((tltrX1, tltrY1), (blbrX1, blbrY1))
 dB1 = dist.euclidean((tlblX1, tlblY1), (trbrX1, trbrY1))
 print(dA1)
 print(dB1)
 dimA1 = dA1 / ppm
 dimB1 = dB1 / ppm
+
+cv2.putText(final_img, "{:.1f}cm".format(dimA1), (int(tltrX1 - 15), int(tltrY1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+cv2.putText(final_img, "{:.1f}cm".format(dimB1), (int(trbrX1 + 10), int(trbrY1)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+
 print("face height: ", float(maxFaceHeight)/ppm, "  face width : ", float(maxFaceWidth)/ppm)
 print("actual height: " , dimA1 , " actual shoulder width: ", dimB1)
 
+cv2.imwrite('processed.png', final_img)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = dir_path + "/processed.png"
 file = open("hw.txt","w")
 file.write(str(dimA1))
 file.write("\n")
 file.write(str(dimB1))
+file.write("\n")
+file.write(dir_path)
 file.close()
 
  # if pixelsPerMetric is None:
